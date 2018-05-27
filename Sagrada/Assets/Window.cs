aa17e2520142535ee387e0e6ade719d30e1d76d6
Jan Andraszyk <jasiek2695@gmail.com>
2018-05-27 12:50:25 +0200

    Divided windows
    Divided windows into smaller pieces for further development

---
+++ Sagrada/Assets/Window.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Window : MonoBehaviour {
+
+    private string windowName;
+    private int difficulty;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
