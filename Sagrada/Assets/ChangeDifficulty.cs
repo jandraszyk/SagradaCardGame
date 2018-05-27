b2d9cf228f83afaba251d7d42dd8316d1ea23bec
Jan Andraszyk <jasiek2695@gmail.com>
2018-05-27 19:20:47 +0200

    Add level difficulty selection

---
+++ Sagrada/Assets/ChangeDifficulty.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeDifficulty : MonoBehaviour {
+
+    private int currentDifficulty = 1;
+    private int maxLevel = 5;
+    private int minLevel = 1;
+    public Text lvlDifficulty;
+
+	public void changeDifficulty(string arrow)
+    {
+        if(arrow.Equals("left"))
+        {
+            if( currentDifficulty > minLevel)
+            {
+                currentDifficulty--;
+                lvlDifficulty.text = currentDifficulty.ToString();
+            }
+        }
+        else if (arrow.Equals("right"))
+        {
+            if(currentDifficulty < maxLevel)
+            {
+                currentDifficulty++;
+                lvlDifficulty.text = currentDifficulty.ToString();
+            }
+        }
+    }
+}
