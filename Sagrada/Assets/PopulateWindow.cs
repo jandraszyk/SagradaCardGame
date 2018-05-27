6b512f87012a3e40acc4cb86b909a35ee3032a41
Jan Andraszyk <jasiek2695@gmail.com>
2018-05-27 18:19:50 +0200

    Changed window generation

---
+++ Sagrada/Assets/PopulateWindow.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopulateWindow : MonoBehaviour {
+
+
+    public int numOfTiles;
+    public GameObject sprite;
+    public string nameOfWindow;
+
+	// Use this for initialization
+	void Start () {
+        Populate();
+	}
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void Populate()
+    {
+
+    GameObject newTile;
+        for(int i=0; i< numOfTiles; i++)
+        {
+            newTile = (GameObject)Instantiate(sprite, transform);
+            newTile.GetComponent<Image>().sprite = Resources.Load<Sprite>("Okna/aurora_sagradis/8");
+        }
+    }
+}
